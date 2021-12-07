97b0cea08fa104faba0e3b039ab20ebaec116d58
AntiZpvoh <wwwqyy@126.com>
2021-12-07 00:40:27 -0800

    add textbox for position inputing

--- happy-link/src/UI.hs
+++ happy-link/src/UI.hs
@@ -23,11 +23,25 @@ import Brick.BChan (newBChan, writeBChan)
 import qualified Brick.Widgets.Border as B
 import qualified Brick.Widgets.Border.Style as BS
 import qualified Brick.Widgets.Center as C
+import qualified Brick.Widgets.Edit as E
+import qualified Brick.AttrMap as A
+import qualified Brick.Focus as F
+import Brick.Util (on)
+import qualified Brick.Types as T
+import Brick.Widgets.Core
+  ( (<+>)
+  , (<=>)
+  , hLimit
+  , vLimit
+  , str
+  )
 import qualified Graphics.Vty as V
 import Data.Sequence (Seq)
 import qualified Data.Sequence as S
 import Linear.V2 (V2(..), _x, _y)
-import Lens.Micro ((^.))
+-- import Lens.Micro ((^.))
+import Lens.Micro
+import Lens.Micro.TH
 
 -- Types
 
@@ -40,15 +54,18 @@ data Tick = Tick
 --
 -- Not currently used, but will be easier to refactor
 -- if we call this "Name" now.
-type Name = ()
+-- type Name = ()
 
 data Cell = Snake | Food | Empty | Bg Char
 
 -- App definition
 
+appCursor :: Game -> [T.CursorLocation Name] -> Maybe (T.CursorLocation Name)
+appCursor = F.focusRingCursor (^.focusRing)
+
 app :: App Game Tick Name
 app = App { appDraw = drawUI
-          , appChooseCursor = neverShowCursor
+          , appChooseCursor = appCursor
           , appHandleEvent = handleEvent
           , appStartEvent = return
           , appAttrMap = const theMap
@@ -69,26 +86,68 @@ main = do
 
 handleEvent :: Game -> BrickEvent Name Tick -> EventM Name (Next Game)
 handleEvent g (AppEvent Tick)                       = continue $ step g
-handleEvent g (VtyEvent (V.EvKey V.KUp []))         = continue $ turn North g
-handleEvent g (VtyEvent (V.EvKey V.KDown []))       = continue $ turn South g
-handleEvent g (VtyEvent (V.EvKey V.KRight []))      = continue $ turn East g
-handleEvent g (VtyEvent (V.EvKey V.KLeft []))       = continue $ turn West g
-handleEvent g (VtyEvent (V.EvKey (V.KChar 'k') [])) = continue $ turn North g
-handleEvent g (VtyEvent (V.EvKey (V.KChar 'j') [])) = continue $ turn South g
-handleEvent g (VtyEvent (V.EvKey (V.KChar 'l') [])) = continue $ turn East g
-handleEvent g (VtyEvent (V.EvKey (V.KChar 'h') [])) = continue $ turn West g
+-- handleEvent g (VtyEvent (V.EvKey V.KUp []))         = continue $ turn North g
+-- handleEvent g (VtyEvent (V.EvKey V.KDown []))       = continue $ turn South g
+-- handleEvent g (VtyEvent (V.EvKey V.KRight []))      = continue $ turn East g
+-- handleEvent g (VtyEvent (V.EvKey V.KLeft []))       = continue $ turn West g
+-- handleEvent g (VtyEvent (V.EvKey (V.KChar 'k') [])) = continue $ turn North g
+-- handleEvent g (VtyEvent (V.EvKey (V.KChar 'j') [])) = continue $ turn South g
+-- handleEvent g (VtyEvent (V.EvKey (V.KChar 'l') [])) = continue $ turn East g
+-- handleEvent g (VtyEvent (V.EvKey (V.KChar 'h') [])) = continue $ turn West g
 handleEvent g (VtyEvent (V.EvKey (V.KChar 'r') [])) = liftIO (initGame) >>= continue
 handleEvent g (VtyEvent (V.EvKey (V.KChar 't') [])) = continue $ eliminate g
 handleEvent g (VtyEvent (V.EvKey (V.KChar 'q') [])) = halt g
 handleEvent g (VtyEvent (V.EvKey V.KEsc []))        = halt g
+handleEvent g (VtyEvent ev) =
+    case ev of
+        V.EvKey V.KEsc [] -> halt g
+        V.EvKey (V.KChar '\t') [] -> continue $ g & focusRing %~ F.focusNext
+        V.EvKey V.KBackTab [] -> continue $ g & focusRing %~ F.focusPrev
+        V.EvKey V.KEnter [] -> continue $ g & focusRing %~ F.focusNext
+
+        _ -> continue =<< case F.focusGetCurrent (g^.focusRing) of
+               Just PosX1 -> T.handleEventLensed g pos_x1 E.handleEditorEvent ev
+               Just PosY1 -> T.handleEventLensed g pos_y1 E.handleEditorEvent ev
+               Just PosX2 -> T.handleEventLensed g pos_x2 E.handleEditorEvent ev
+               Just PosY2 -> T.handleEventLensed g pos_y2 E.handleEditorEvent ev
+               Nothing -> return g
 handleEvent g _                                     = continue g
 
 -- Drawing
 
+-- data Name = Edit1
+--           | Edit2
+--           deriving (Ord, Show, Eq)
+
+-- data St =
+--     St { _focusRing :: F.FocusRing Name
+--        , _edit1 :: E.Editor String Name
+--        , _edit2 :: E.Editor String Name
+--        }
+
+-- initialState :: St
+-- initialState =
+--     St (F.focusRing [Edit1, Edit2])
+--        (E.editor Edit1 Nothing "")
+--        (E.editor Edit2 (Just 2) "")
+
+-- makeLenses ''St
+
 drawUI :: Game -> [Widget Name]
 drawUI g =
-  [ C.center $ padRight (Pad 2) (drawStats g) <+> drawGrid g ]
+  [ C.center $ padRight (Pad 2) (drawStats g) <+> drawGrid g <+> drawInput g]
+    
 
+drawInput :: Game -> Widget Name
+drawInput g = hLimit 15
+  $ vBox [ hLimit 30 (vLimit 5 px1) , hLimit 30 (vLimit 5 py1)
+         , hLimit 30 (vLimit 5 px2) , hLimit 30 (vLimit 5 py2)
+         ]
+  where
+    px1 = F.withFocusRing (g ^. focusRing) (E.renderEditor (str . unlines)) (g ^. pos_x1)
+    px2 = F.withFocusRing (g ^. focusRing) (E.renderEditor (str . unlines)) (g ^. pos_x2)
+    py1 = F.withFocusRing (g ^. focusRing) (E.renderEditor (str . unlines)) (g ^. pos_y1)
+    py2 = F.withFocusRing (g ^. focusRing) (E.renderEditor (str . unlines)) (g ^. pos_y2)
 
 drawStats :: Game -> Widget Name
 drawStats g = hLimit 11
@@ -149,4 +208,6 @@ theMap = attrMap V.defAttr
   [ (snakeAttr, V.blue `on` V.blue)
   , (foodAttr, V.red `on` V.red)
   , (gameOverAttr, fg V.red `V.withStyle` V.bold)
+  , (E.editAttr,                   V.white `on` V.black)
+  , (E.editFocusedAttr,            V.black `on` V.white)
   ]
