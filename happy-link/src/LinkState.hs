20a543e32aba36eefd68f3ef66292b434d302bb2
tongyanjun <yj.sjtu@gmail.com>
2021-12-09 18:50:36 -0800

    add padding

--- happy-link/src/LinkState.hs
+++ happy-link/src/LinkState.hs
@@ -5,16 +5,13 @@ module LinkState
   ( initGame
   , step
   , turn
-  -- , eliminate
+  , eliminate
   , Game(..)
   , Direction(..)
-  , dead, food, score, snake, cells, info, linkable
+  , dead, food, score, snake, blocks
   , focusRing, pos_x1, pos_y1, pos_x2, pos_y2
-  , lastReportedClick
   , height, width
   , Name(..)
-  , link, isLinkable
-  , shuffleGame
   ) where
 
 import Data.Array.IO
@@ -25,7 +22,6 @@ import Data.Maybe (fromMaybe)
 import qualified Brick.Widgets.Edit as E
 import qualified Brick.AttrMap as A
 import qualified Brick.Focus as F
-import qualified Brick.Types as T
 import Brick.Util (on)
 
 import Control.Lens hiding ((<|), (|>), (:>), (:<))
@@ -36,6 +32,7 @@ import Data.Sequence (Seq(..), (<|))
 import qualified Data.Sequence as S
 import Linear.V2 (V2(..), _x, _y)
 import System.Random (Random(..), newStdGen, randomRIO)
+import Prelude
 
 -- Types
 
@@ -43,7 +40,6 @@ data Name = PosX1
           | PosY1
           | PosX2
           | PosY2
-          | Board
           deriving (Ord, Show, Eq)
 
 data Game = Game
@@ -56,16 +52,12 @@ data Game = Game
   , _score  :: Int          -- ^ score
   , _locked :: Bool         -- ^ lock to disallow duplicate turns between time steps
   , _blocks :: [Char]
-  , _cells  :: [[Char]]
   , _input  :: Bool
   , _focusRing :: F.FocusRing Name
   , _pos_x1 :: E.Editor String Name
   , _pos_y1 :: E.Editor String Name
   , _pos_x2 :: E.Editor String Name
   , _pos_y2 :: E.Editor String Name
-  , _lastReportedClick :: Maybe (Name, T.Location)
-  , _linkable :: Bool
-  , _info :: [Char]
   -- } deriving (Show)
   }
 
@@ -103,8 +95,8 @@ step s = flip execState s . runMaybeT $ do
   -- die (moved into boundary), eat (moved into food), or move (move into space)
   die <|> eatFood <|> MaybeT (Just <$> modify move)
 
--- eliminate :: Game -> Game
--- eliminate g = g & blocks .~ (' ' : (tail (g ^. blocks)))
+eliminate :: Game -> Game
+eliminate g = g & blocks .~ (' ' : (tail (g ^. blocks)))
 
 -- | Possibly die if next head position is in snake
 die :: MaybeT (State Game) ()
@@ -157,29 +149,6 @@ turnDir n c | c `elem` [North, South] && n `elem` [East, West] = n
             | c `elem` [East, West] && n `elem` [North, South] = n
             | otherwise = c
 
-replaceAtIndex :: Int -> a -> [a] -> [a]
-replaceAtIndex n item ls = a ++ (item:b) where (a, (_:b)) = splitAt n ls
-
-link :: (Int, Int) -> (Int, Int) -> Game -> Game
-link (x1, y1) (x2, y2) g@Game {_cells = cells_old, _blocks = blocks_old, _score = s}
-  | isLinkable cells_old x1 y1 x2 y2 = do
-    -- let x1Row = cells_old !! x1
-    -- let cells_tmp = x1Row & element y1 .~ ' '
-    -- let cells_new_1 = cells_old & element x1 .~ cells_tmp
-    -- let x2Row = cells_new_1 !! x2
-    -- let cells_tmp_2 = x2Row & element y2 .~ ' '
-    -- let cells_new_2 = cells_new_1 & element x2 .~ cells_tmp_2
-    let blocks_new_1 = blocks_old & element (x1 * width + y1) .~ ' '
-    let blocks_new_2 = blocks_new_1 & element (x2 * width + y2) .~ ' '
-    let from_list = iterate (width+) 0
-    let rb_list = map (dropFrom blocks_new_2) from_list
-    let cells_new_2 = take height (map (take width) rb_list)
-    let s_new = s + 1
-    g & cells .~ cells_new_2
-      & score .~ s_new
-      & blocks .~ blocks_new_2
-  | otherwise = g
-
 shuffle :: [a] -> IO [a]
 shuffle xs = do
         ar <- newArray n xs
@@ -193,19 +162,7 @@ shuffle xs = do
     n = length xs
     newArray :: Int -> [a] -> IO (IOArray Int a)
     newArray n xs =  newListArray (1,n) xs
-
-shuffleGame :: Game -> IO Game
-shuffleGame g@Game {_blocks = blocks_old} = do
-  b <- shuffle blocks_old
-  let from_list = iterate (width+) 0
-  let rb_list = map (dropFrom b) from_list
-  let cells_new = take height (map (take width) rb_list)
-  return (g & cells .~ cells_new
-            & blocks .~ b)
-
-dropFrom :: [Char] -> Int -> [Char]
-dropFrom l f = drop f l
-
+  
 -- | Initialize a paused game with random food location
 initGame :: IO Game
 initGame = do
@@ -213,10 +170,6 @@ initGame = do
   rb <- randomRs ('A', 'Z') <$> newStdGen
   let b = take (quot (width * height) 2) rb
   blocks <- shuffle $ b ++ b
-  let from_list = iterate (width+) 0
-  let rb_list = map (dropFrom blocks) from_list
-  let cells = take height (map (take width) rb_list)
-  -- blocks <- shuffle b
   let xm = width `div` 2
       ym = height `div` 2
       g  = Game
@@ -229,16 +182,11 @@ initGame = do
         , _paused = True
         , _locked = False
         , _blocks = blocks
-        -- , _cells  = [[' ',' ',' ',' '],[' ',' ',' ',' '],['O',' ',' ',' '],['B','O','B',' ']]
-        , _cells = cells
         , _focusRing = F.focusRing [PosX1, PosY1, PosX2, PosY2]
         , _pos_x1 = E.editor PosX1 (Just 2) ""
         , _pos_y1 = E.editor PosY1 (Just 2) ""
         , _pos_x2 = E.editor PosX2 (Just 2) ""
         , _pos_y2 = E.editor PosY2 (Just 2) ""
-        , _lastReportedClick = Nothing
-        , _linkable = False
-        , _info = "no"
         }
   return $ execState nextFood g
 
@@ -248,7 +196,7 @@ fromList = foldr (:|) (error "Streams must be infinite")
 -- | Check if two points are linkable
 
 isEmptyRow :: [Char] -> Int -> Int -> Bool
-isEmptyRow nums l h =
+isEmptyRow nums l h = 
   if l > h then True -- base
   else if nums !! l /= ' ' then False -- base
   else isEmptyRow nums (l + 1) h -- n-1 -> n
@@ -263,7 +211,7 @@ isEmptyCol g col l h =
 -- True
 --
 isLinkable0 :: [[Char]] -> Int -> Int -> Int -> Int -> Bool
-isLinkable0 g row1 col1 row2 col2 =
+isLinkable0 g row1 col1 row2 col2 = 
   if row1 == row2 && col1 == col2 then False -- filters on same non-space character placed outside
   else if row1 /= row2 && col1 /= col2 then False
   else if row1 == row2 then isEmptyRow (g !! row1) ((min col1 col2) + 1) ((max col1 col2) - 1)
@@ -274,7 +222,7 @@ isLinkable0 g row1 col1 row2 col2 =
 -- True
 --
 isLinkable1 :: [[Char]] -> Int -> Int -> Int -> Int -> Bool
-isLinkable1 g row1 col1 row2 col2 =
+isLinkable1 g row1 col1 row2 col2 = 
   if row1 == row2 && col1 == col2 then False -- filters on same non-space character placed outside
   else if row1 == row2 || col1 == col2 then False
   else if g !! row1 !! col2 == ' ' && (isLinkable0 g row1 col1 row1 col2) && (isLinkable0 g row1 col2 row2 col2) then True
@@ -289,41 +237,41 @@ isLinkable1 g row1 col1 row2 col2 =
 -- True
 --
 hasLinkable1Right :: [[Char]] -> Int -> Int -> Int -> Int -> Int -> Bool
-hasLinkable1Right g row1 row2 col2 l h =
+hasLinkable1Right g row1 row2 col2 l h = 
   if l > h then False -- base
   else if (g !! row1 !! l /= ' ') then False -- base
   else if (g !! row1 !! l == ' ') && isLinkable1 g row1 l row2 col2 then True -- base
   else hasLinkable1Right g row1 row2 col2 (l + 1) h -- n-1 -> n
 
 hasLinkable1Left :: [[Char]] -> Int -> Int -> Int -> Int -> Int -> Bool
-hasLinkable1Left g row1 row2 col2 l h =
+hasLinkable1Left g row1 row2 col2 l h = 
   if l > h then False -- base
   else if (g !! row1 !! h /= ' ') then False -- base
   else if (g !! row1 !! h == ' ') && isLinkable1 g row1 h row2 col2 then True -- base
   else hasLinkable1Left g row1 row2 col2 l (h - 1) -- n-1 -> n
 
 hasLinkable1Down :: [[Char]] -> Int -> Int -> Int -> Int -> Int -> Bool
-hasLinkable1Down g col1 row2 col2 l h =
+hasLinkable1Down g col1 row2 col2 l h = 
   if l > h then False -- base
   else if (g !! l !! col1 /= ' ') then False -- base
   else if (g !! l !! col1 == ' ') && isLinkable1 g l col1 row2 col2 then True -- base
   else hasLinkable1Down g col1 row2 col2 (l + 1) h -- n-1 -> n
 
 hasLinkable1Up :: [[Char]] -> Int -> Int -> Int -> Int -> Int -> Bool
-hasLinkable1Up g col1 row2 col2 l h =
+hasLinkable1Up g col1 row2 col2 l h = 
   if l > h then False -- base
   else if (g !! h !! col1 /= ' ') then False -- base
   else if (g !! h !! col1 == ' ') && isLinkable1 g h col1 row2 col2 then True -- base
   else hasLinkable1Up g col1 row2 col2 l (h - 1) -- n-1 -> n
 
 isLinkable2 :: [[Char]] -> Int -> Int -> Int -> Int -> Bool
-isLinkable2 g row1 col1 row2 col2 =
+isLinkable2 g row1 col1 row2 col2 = 
   if row1 == row2 && col1 == col2 then False -- filters on same non-space character placed outside
-  else hasLinkable1Right g row1 row2 col2 (col1 + 1) (length (g !! row1) - 1) ||
+  else hasLinkable1Right g row1 row2 col2 (col1 + 1) (length (g !! row1) - 1) || 
     hasLinkable1Left g row1 row2 col2 0 (col1 - 1) ||
     hasLinkable1Down g col1 row2 col2 (row1 + 1) (length g - 1) ||
     hasLinkable1Up g col1 row2 col2 0 (row1 - 1)
-
+    
 
 -- >>> isLinkable [[' ',' ',' ','D'],[' ','C','C','E'],[' ',' ',' ','D']] 0 3 2 3
 -- True
@@ -339,9 +287,19 @@ isLinkable2 g row1 col1 row2 col2 =
 -- True
 --
 isLinkable :: [[Char]] -> Int -> Int -> Int -> Int -> Bool
-isLinkable g row1 col1 row2 col2 =
+isLinkable g row1 col1 row2 col2 = 
   if row1 == row2 && col1 == col2 || ((g !! row1 !! col1) /= (g !! row2 !! col2)) || (g !! row1 !! col1 == ' ') then False
   else (isLinkable0 g row1 col1 row2 col2) || (isLinkable1 g row1 col1 row2 col2) || (isLinkable2 g row1 col1 row2 col2)
 
-isLinkableStub :: [[Char]] -> Int -> Int -> Int -> Int -> Bool
-isLinkableStub g row1 col1 row2 col2 = True
+padding :: [[Char]] -> [[Char]]
+padding g@(cs:css) = (zeros : (map padSides g)) ++ [zeros]
+  where zeros = buildZeros n
+        n     = (length cs) + 2
+
+padSides :: [Char] -> [Char]
+padSides cs = (' ' : cs) ++ " "
+
+buildZeros :: Int -> [Char]
+buildZeros n = Prelude.replicate n ' '
+
+-- >>> padding [['A', 'B', 'C'], ['A', 'B', 'C'], ['A', 'B', 'C']]
